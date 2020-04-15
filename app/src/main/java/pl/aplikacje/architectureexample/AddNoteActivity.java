96a068a13be9b5b4ddb0304b24df027f2163e4b1
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-04-15 13:41:06 +0200

    recycler_adapter_viewHolder + 2

---
+++ app/src/main/java/pl/aplikacje/architectureexample/AddNoteActivity.java
@@ -0,0 +1,31 @@
+package pl.aplikacje.architectureexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+    }
+}
